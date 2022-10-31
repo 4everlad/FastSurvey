5f44c854c9d7c7881ea63938159107971903093c
4everlad <d.s.bakulin@gmail.com>
2022-10-31 15:55:00 +0300

    Initial Commit

---
+++ FastSurvey/FastSurveyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastSurveyApp.swift
+//  FastSurvey
+//
+//  Created by Dmitry Bakulin on 31.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct FastSurveyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
