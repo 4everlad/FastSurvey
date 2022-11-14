72236dec47e6d7476633da1f638770d715c95f02
4everlad <d.s.bakulin@gmail.com>
2022-11-14 12:08:06 +0300

    fix ontapgesture

--- FastSurvey/SurveyFeed/Views/SurveyListView.swift
+++ FastSurvey/SurveyFeed/Views/SurveyListView.swift
@@ -19,7 +19,7 @@ struct SurveysListView: View, IItemView {
             ForEach(viewModel.surveys) { item in
                 let isElementLast = viewModel.surveys.isLast(item)
                 SurveyCellView(survey: item)
-                    .onTapGesture {
+                    .expandTap {
                         listener?.push(view: SurveyScreenView(surveyID: item.sid))
                     }
                     .onAppear {
@@ -35,5 +35,23 @@ struct SurveysListView: View, IItemView {
         .onAppear {
             viewModel.getSurveys()
         }
+    }
+}
+
+
+private struct ExpandAreaTap: ViewModifier {
+    func body(content: Content) -> some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(Color.white)
+                .contentShape(Rectangle())
+            content
+        }
+    }
+}
+
+extension View {
+    func expandTap(tap: @escaping () -> ()) -> some View {
+        self.modifier(ExpandAreaTap()).onTapGesture(perform: tap)
     }
 }
