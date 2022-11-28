9dbd2b2536a6264d410f702f2feec8e1d95ea5c3
4everlad <d.s.bakulin@gmail.com>
2022-11-28 09:54:24 +0300

    update profile UI

--- FastSurvey/UserAccount/UserAccountScreenView.swift
+++ FastSurvey/UserAccount/UserAccountScreenView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import SwiftUINavigator
+import Introspect
 
 struct UserAccountScreenView: View, IItemView {
     
@@ -16,36 +17,81 @@ struct UserAccountScreenView: View, IItemView {
     var body: some View {
         NavigationView {
             VStack {
-                Form {
-                    HStack {
-                        Text("Name")
-                        Spacer()
-                        Text(viewModel.user.name)
+                List {
+                    Section {
+                        VStack {
+                            HStack{
+                                Text("Name")
+                                    .font(.subheadline)
+                                Spacer()
+                                Text(viewModel.user.name)
+                                    .font(.subheadline)
+                                    .fontWeight(.semibold)
+                                    .foregroundColor(.blue)
+                            }
+                            .padding(8)
+                            Divider()
+                                .overlay(.blue)
+                            
+                            HStack {
+                                Text("Email")
+                                    .font(.subheadline)
+                                Spacer()
+                                Text(viewModel.user.email)
+                                    .font(.subheadline)
+                                    .fontWeight(.semibold)
+                                    .foregroundColor(.blue)
+                            }
+                            .padding(8)
+                            Divider()
+                                .overlay(.blue)
+                            
+                            HStack {
+                                Text("Age")
+                                    .font(.subheadline)
+                                Spacer()
+                                Text(viewModel.user.age)
+                                    .font(.subheadline)
+                                    .fontWeight(.semibold)
+                                    .foregroundColor(.blue)
+                            }
+                            .padding(8)
+                            Divider()
+                                .overlay(.blue)
+                            
+                            HStack {
+                                Text("Gender")
+                                    .font(.subheadline)
+                                Spacer()
+                                Text(viewModel.user.sex)
+                                    .font(.subheadline)
+                                    .fontWeight(.semibold)
+                                    .foregroundColor(.blue)
+                            }
+                            .padding(8)
+                            Divider()
+                                .overlay(.blue)
+                            
+                            HStack {
+                                Text("Country")
+                                    .font(.subheadline)
+                                Spacer()
+                                Text(viewModel.user.countryCode)
+                                    .font(.subheadline)
+                                    .fontWeight(.semibold)
+                                    .foregroundColor(.blue)
+                            }
+                            .padding(8)
+                        }
+                        .background(.blue.opacity(0.1))
+                        .overlay(RoundedRectangle(cornerRadius: 10, style: .circular).stroke(.blue.opacity(0.1)))
+                        .listRowSeparator(.hidden)
                     }
                     
-                    HStack {
-                        Text("Email")
-                        Spacer()
-                        Text(viewModel.user.email)
-                    }
-                    
-                    HStack {
-                        Text("Age")
-                        Spacer()
-                        Text(viewModel.user.age)
-                    }
-                    
-                    HStack {
-                        Text("Gender")
-                        Spacer()
-                        Text(viewModel.user.sex)
-                    }
-                    
-                    HStack {
-                        Text("Country")
-                        Spacer()
-                        Text(viewModel.user.countryCode)
-                    }
+                }
+                .listStyle(InsetListStyle())
+                .onAppear {
+                    UITableView.appearance().backgroundColor = .clear
                 }
             }
             .navigationBarItems(trailing:
